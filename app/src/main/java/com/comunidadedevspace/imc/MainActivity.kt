e948bb445239a0ab24b7c4dacd142124be4797d7
limapedro3108 <limapedro3108@gmail.com>
2024-12-05 19:20:08 +0000

    Second commit
    Signed-off-by: Pedro Lima <limapedro3108@gmail.com>

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,11 +1,32 @@
-package com.comunidadedevspace.imc
+package com.example.primeiroapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Vou ser o melhor programador Kotlin do mundo 01/12/2024.
+
+        val edtpeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtaltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso = edtpeso.text
+            val altura = edtaltura.text
+            println("Pedro acao do botao " + altura)
+        }
+
+
     }
 }
